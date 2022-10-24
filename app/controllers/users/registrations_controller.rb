3e2c8fe60ce4f89dcacd5ebbe53dfe68834a0721
safar ali <miqra019@gmail.com>
2022-10-24 23:01:27 +0500

    Add controller for loging in

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,15 +1,18 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+
  respond_to :json
+
+ private
 
  def respond_with(resource, options={})
    if resource.persisted?
       render json: {
-        status: {code: 200, message: "Sign up successful", data: resource}
+        status: {code: 200, message: "Sign up successful", data: resource}, status: :ok
       } else
        render json: {
-        message: "Person couldn't be created", error: resource.errors.full_message
+        message: "Person couldn't be created", error: resource.errors.full_messages
        }, status: unprocessable_entity
    end
  end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,27 +1,31 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+ 
+  respond_to :json
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  private
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def respond_with(resource, options={})
+  
+  render json: {
+    status: {code: 200, message: "User sign in successfull", data: current_user}, status: :ok
+  }
+end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+def respond_to_on_destroy
+  jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1], Rails.Application.credentials.fetch(:secret_key_base)).first
+  current_user = User.find(jwt_payload['sub'])
+  if current_user
+    render :json {
+      status: 200, message: "User signed out successfully"
+    }, status: :ok
+  else
+    render :json{
+      status: 401, message "User has no active sessions"
+    }, status: unathourized
+  end
+end
 
-  # protected
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
