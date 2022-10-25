680d22d98017efaf795009693e225bff683f859d
Olawale <olapetanwale@outlook.com>
2022-10-25 16:54:25 +0100

    Configure devise to permit username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  before_action :permit_params
+
+    private
+
+    def permit_params
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password])
+    end
+
 end
