dc93299edc212a3ef717ae8595ee87feb4f4f7d4
safar ali <miqra019@gmail.com>
2022-10-25 15:51:35 +0500

    Solve api issue

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -9,8 +9,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
  def respond_with(resource, options={})
    if resource.persisted?
       render json: {
-        status: {code: 200, message: "Sign up successful", data: resource}, status: :ok
-      } else
+        status: {code: 200, message: "Signed up successfully", data: resource}
+      }, status: :ok
+    else
        render json: {
         message: "Person couldn't be created", error: resource.errors.full_messages
        }, status: unprocessable_entity
