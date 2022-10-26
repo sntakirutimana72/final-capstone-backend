5eff1ebcea1753fdfb7051f3c4c391184a9806fc
Chris Siku <chrissiku5@gmail.com>
2022-10-26 10:30:16 +0200

    Update readme file and setup a master new key

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -309,7 +309,7 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
   config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.fetch(:jwt_secret_key)
+    jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
     jwt.dispatch_requests = [
       ['POST', %r{^/users/sign_in$}]
       # ['POST', %r{^/api/v1/auth/sign_in$}]
