39af08878cfe3e3628cde3a9436713fa24a522dc
Olawale <olapetanwale@outlook.com>
2022-10-26 15:37:11 +0100

    Add Accomadations model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_26_140936) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_26_142933) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accomodations", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "reservations", force: :cascade do |t|
     t.bigint "room_id", null: false
