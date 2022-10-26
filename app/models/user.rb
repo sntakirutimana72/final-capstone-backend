667275d2e48674025e097b4db7050d39fe8b76c1
safar ali <miqra019@gmail.com>
2022-10-26 18:50:34 +0500

    Create tables for rooms and room types

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
   belongs_to :role
+  has_many :rooms
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_26_120747) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_26_133239) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,25 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_26_120747) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "room_types", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "number_of_beds", null: false
+    t.decimal "price", null: false
+    t.text "description"
+    t.bigint "room_type_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_type_id"], name: "index_rooms_on_room_type_id"
+    t.index ["user_id"], name: "index_rooms_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,5 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_26_120747) do
     t.index ["role_id"], name: "index_users_on_role_id"
   end
 
+  add_foreign_key "rooms", "room_types"
+  add_foreign_key "rooms", "users"
   add_foreign_key "users", "roles"
 end
