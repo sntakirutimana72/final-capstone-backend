37c1d919b8660c2453fe35bcb9d0f001de1dc59f
safar ali <miqra019@gmail.com>
2022-10-26 19:03:30 +0500

    Solve linters errors

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,8 +3,7 @@ class Room < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true, uniqueness: true
-  validates :number_of_beds, presence: true,  numericality: { only_integer: true, greater_than: 0 }
+  validates :number_of_beds, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :description, length: { maximum: 900 }
-  
 end
