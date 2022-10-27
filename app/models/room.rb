a479dfc67aaf0d2b498e4bfb352b922d6a2a4238
aliatayee <aliaqaatayee@gmail.com>
2022-10-27 14:51:06 +0430

    Update room model

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,6 +3,8 @@ class Room < ApplicationRecord
   belongs_to :user
   has_many :reservations
   has_many :users, through: :reservations
+  has_many :room_accomodations
+  has_many :accomodations, through: :room_accomodations
 
   validates :name, presence: true, uniqueness: true
   validates :number_of_beds, presence: true, numericality: { only_integer: true, greater_than: 0 }
