3b1e0a197132d741e5412602d4176a2a601f3da8
Chris Siku <chrissiku5@gmail.com>
2022-10-31 16:16:49 +0200

    Clean the rooms controller

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,3 +1,21 @@
 class ApiController < ApplicationController
+  # --------------------------------------------------------------------------
+  # |  Uncomment this line if when you have a function authentication system |
+  # --------------------------------------------------------------------------
+
   # before_action :authenticate_user!
+
+  private
+
+  def room_to_json(room)
+    {
+      id: room.id,
+      name: room.name,
+      picture: room.picture,
+      number_of_beds: room.number_of_beds,
+      price: room.price,
+      description: room.description,
+      room_type: room.room_type.name
+    }
+  end
 end
