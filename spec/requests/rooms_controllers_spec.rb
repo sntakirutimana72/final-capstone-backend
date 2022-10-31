4c01b89813e89e336fb59a77531c8c52099e0cba
Chris Siku <chrissiku5@gmail.com>
2022-10-31 16:36:25 +0200

    Write test for the index routes on RoomsController

--- spec/requests/rooms_controllers_spec.rb
+++ spec/requests/rooms_controllers_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'Rooms', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'returns http success' do
+      get '/api/v1/rooms'
+      expect(response).to have_http_status(:success)
+    end
   end
 end
