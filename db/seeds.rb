9219d4a41ce616e04bfa012c10c96f05f3c33312
aliatayee <aliaqaatayee@gmail.com>
2022-11-01 10:34:31 +0430

    Update seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,10 @@
 # 5.times do |i|
 #   RoomType.create(name: "#{i +1} star")
 # end
+role = Role.create(name: 'admin')
+user = User.create(username: 'Ali',email:'ali@gmail.com',password:'123456', role:role)
+roomType = RoomType.create(name: 'test')
+Room.create(name: 'room1',number_of_beds:2,price:200,description:'details',room_type: roomType,user:user)
 
 10.times do |i|
   Accomodation.create(name: "#{i +1}")
