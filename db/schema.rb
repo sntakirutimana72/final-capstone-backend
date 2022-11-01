c27e27c444610fd95d0a3cc803efcdd5a9f5ad7b
aliatayee <aliaqaatayee@gmail.com>
2022-11-01 10:36:06 +0430

    Update reservations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_27_094357) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_27_180306) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_094357) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "from_date"
+    t.date "to_date"
     t.index ["room_id"], name: "index_reservations_on_room_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
