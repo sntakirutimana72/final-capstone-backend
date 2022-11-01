a00ebee359c5ace1fc54254208c90b403d87a90e
Steve <sntakirutimana72@gmail.com>
2022-11-01 11:02:47 +0200

    Adjust previous app setup in sync with new changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,14 @@ class ApplicationController < ActionController::API
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password) }
   end
 
-  def user_to_json(user)
-    {
-      username: user.username,
-      email: user.email,
-      role: user.role.name
-    }
+  def respond_not_found
+    render(json: { error: 'Not Found', status: 404 }, status: :not_found)
+  end
+
+  def respond_unprocessable(resource)
+    render(
+      json: { status: 422, error: resource.errors.objects.first.full_message },
+      status: :unprocessable_entity
+    )
   end
 end
