745b8414e67484f196a970f85dce8e8d84985a7a
aliatayee <aliaqaatayee@gmail.com>
2022-11-01 10:34:09 +0430

    Add reservation api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :reservations, only: [:create, :index, :show, :destroy]
+    end
+  end
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
