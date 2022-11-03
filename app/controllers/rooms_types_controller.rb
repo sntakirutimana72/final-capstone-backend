8aba864703b5cb498a2775f32aacae39f742906c
Steve <sntakirutimana72@gmail.com>
2022-11-03 10:57:35 +0200

    Fix linter errors

--- app/controllers/rooms_types_controller.rb
+++ app/controllers/rooms_types_controller.rb
@@ -1,8 +1,6 @@
 class RoomsTypesController < ApplicationController
-
   def index
     @rooms_types = RoomType.all
     render json: { rooms_types: @rooms_types.map { |type| room_type_to_json(type) } }, status: :ok
   end
-
 end
